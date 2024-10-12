ef41372513da499410bb02e22145408050ddbf45
hoshi <chlehdrb4967@gmail.com>
2024-10-13 00:09:13 +0900

    Update

--- Assets/Scripts/UIScripts/ChangePlayerName.cs
+++ Assets/Scripts/UIScripts/ChangePlayerName.cs
@@ -7,14 +7,25 @@ using UnityEngine.UI;
 public class ChangePlayerName : MonoBehaviour
 {
     public InputField NameField;
-    private PlayerNameDisplay playerNameDisplay;
     
+    public Button Btn;
+
+    private PlayerNameDisplay playerNameDisplay;
+
+    private void Start()
+    {
+        Btn.onClick.AddListener(ChangeName);
+
+        playerNameDisplay = FindObjectOfType<PlayerNameDisplay>();
+    }
     public void ChangeName()
     {
         string newName = NameField.text;
         DataManager.Instance.SettingPlayerName(newName);
-
-        playerNameDisplay.UpdatePlayerName(newName);
+        if (playerNameDisplay != null)
+        {
+            playerNameDisplay.UpdatePlayerName(newName);
+        }
 
         Debug.Log($"이름이 바뀌었습니다.: {newName}");
     }
