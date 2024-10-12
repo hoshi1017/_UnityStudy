02b5b5fe4f5dfc5ba81cbabb3f5cd77cc7fd1955
hoshi <chlehdrb4967@gmail.com>
2024-10-12 15:38:31 +0900

    Update

--- Assets/Scripts/Player/PlayerNameDisplay.cs
+++ Assets/Scripts/Player/PlayerNameDisplay.cs
@@ -29,7 +29,7 @@ public class PlayerNameDisplay : MonoBehaviour
         if (player != null && playerNameText != null)
         {
             
-            Vector3 offset = new Vector3(0, 2f, 0); 
+            Vector3 offset = new Vector3(0, 1.8f, 0); 
             playerNameText.transform.position = Camera.main.WorldToScreenPoint(player.transform.position + offset);
         }
     }
